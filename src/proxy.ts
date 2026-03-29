fdca0a9fe388f882ba90a3585e4f10bf3d316010
mootracker-hu <hellocsillu@gmail.com>
2026-03-29 17:56:30 +0200

    debug(routing): remove proxy.ts to test direct routing

--- src/proxy.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse, type NextRequest } from 'next/server'
-
-export async function proxy(request: NextRequest) {
-  return NextResponse.next()
-}
-
-export const config = {
-  matcher: '/:path*',
-}
